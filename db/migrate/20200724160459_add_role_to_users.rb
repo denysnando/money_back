b22ebc9e3c39d6b4b7e2f5fb347c7352bf4e0e64
Denys <denysnando@gmail.com>
2020-11-05 15:00:35 -0300

    change name div and some fix in migrate

--- db/migrate/20200724160459_add_role_to_users.rb
+++ db/migrate/20200724160459_add_role_to_users.rb
@@ -2,7 +2,6 @@
 
 class AddRoleToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :role, :integer
     add_column :users, :user_type, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_11_04_194406) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.integer "role"
     t.string "user_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
